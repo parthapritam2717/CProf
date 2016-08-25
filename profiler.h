f50e3154623e8e452f86a5585665e3f72cb03fed
partha.lab <parthapritam2717@hotmail.com>
2016-08-25 20:00:17 +0530

    Notes updated and profiler memory mapping working

---
+++ profiler.h
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+int getVirtualMemoryUsed();//done
+int getCpuUsed();//Nee to implement from some other program as in the same program its unable to monitor
+int getPhysicalMemoryUsed();//done
