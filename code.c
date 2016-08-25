fe9eba9493a174cca94965dd79dd39fde54a363c
partha.lab <parthapritam2717@hotmail.com>
2016-08-25 16:14:01 +0530

    upto #defines written code

---
+++ code.c
@@ -0,0 +1,18 @@
+/***********************************All the external include files please write here********************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<stdbool.h>
+
+/**************************************************Includes ends here*************************************************/
+
+/*******************************************All #defines please include here ******************************************/
+#define nl() printf("\n");
+#define os() printf(" ");
+#define sf(n) scanf("%d",&n);
+#define pf(n) printf("%d",n);
+
+/*********************************************#defines ends here******************************************************/
+
+
+
