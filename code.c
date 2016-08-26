c1e3bdc4350eefe8322984bd46eed2f955443fc2
partha <parthapritam2717@hotmail.com>
2016-08-26 14:54:01 +0530

    code file updated added prime functions so for ease of development and keeping track of the basic blocks

--- code.c
+++ code.c
@@ -24,6 +24,29 @@
 
 
 /***************************************Global variables declaration ends here***************************************/
+bool is_prime(int item){	
+	double y=(double)item;
+	double x=sqrt(y);
+	int i;
+	int end=(int)x;
+	for(i=2;i<(int)x;++i){
+		if(item%i==0){
+			return 0;
+		}
+		
+	}
+	return 1;
+
+}
+int count_prime(int *array,int s){
+	int i=0,j=0;	
+	for(i=0;i<s;++i){
+		if(array[i]>=1 && is_prime(array[i])){
+			j++;
+		}
+	}
+	return j;
+}
 
 int main(){
 	int n;
